e5865fd9f099c57a31579216c39c4ef798d176de
Chris Maxwell <chmaxwel@icloud.com>
2020-11-27 22:17:56 +0100

    jaws_db for toothless

---
+++ config/connection.js
@@ -0,0 +1,15 @@
+// Dependencies
+const Sequelize = require('sequelize');
+
+// Creates mySQL connection using Sequelize
+// Include your MySQL user/password information
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      port: 3306,
+      dialect: 'mysql'
+    });
+
+// Exports the connection for other files to use
+module.exports = sequelize;
