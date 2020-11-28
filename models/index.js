1ec2dea5953247717d4495be0eb3ec2651f8ab59
Chris Maxwell <chmaxwel@icloud.com>
2020-11-28 20:08:07 +0100

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,5 +1,11 @@
-const Keywords = require('./keywords');
-const Toothless = require('./toothless');
+const Keywords = require('./Keywords');
+const Toothless = require('./Toothless');
+
+// Products belongsTo Category
+Keywords.belongsTo(Toothless, {
+    foreignKey: 'toothless_id',
+    onDelete: 'SET NULL'
+  })
 
 // Categories have many Products
 Toothless.hasMany(Keywords, {
