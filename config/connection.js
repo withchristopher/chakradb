a6acea30eee9d1a68de2dbede1cc82af9fabde7f
Chris Maxwell <chmaxwel@icloud.com>
2020-11-28 20:36:48 +0100

    update

--- config/connection.js
+++ config/connection.js
@@ -3,6 +3,8 @@ const Sequelize = require('sequelize');
 
 // Creates mySQL connection using Sequelize
 // Include your MySQL user/password information
+require('dotenv').config();
+
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
